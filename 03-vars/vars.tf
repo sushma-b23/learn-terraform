32ebbb44f286ff5bc6519793611324d58c3e106c
sushma-b23 <susmaboothu5@gmail.com>
2023-09-22 14:52:13 +0530

    jenkins

--- 03-vars/vars.tf
+++ 03-vars/vars.tf
@@ -5,3 +5,58 @@ variable "sample" {
 output "sample" {
   value = var.sample
 }
+
+# String Data type
+variable "sample1" {
+  default = "Hello World"
+}
+
+# Number Data type
+variable "sample2" {
+   default = 100
+ }
+
+# Boolean Data type
+variable "sample3" {
+  default = true
+}
+
+# Default Variable type
+variable "sample4" {
+  default = 100
+ }
+output "sample4" {
+  value = var.sample4
+ }
+
+# List Variable type
+variable "sample5" {
+  default = [
+    100,
+    "abc",
+    "xyz",
+    ]
+   }
+
+output "sample5" {
+ value = var.sample5[1]
+ }
+
+# Map Variable type
+variable "sample6" {
+   default = {
+      number = 100
+      string = "xyz"
+      boolean = false
+ }
+}
+
+output "sample6" {
+  value = var.sample6["number"]
+  }
+
+
+
+
+
+
