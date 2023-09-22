444476385c8f7bfba27a5b69172309004f0335b4
sushma-b23 <susmaboothu5@gmail.com>
2023-09-22 14:37:18 +0530

    jenkins

--- 02-data-sources/data.tf
+++ 02-data-sources/data.tf
@@ -6,3 +6,10 @@ data "aws_ami" "example" {
 output "ami" {
   value = data.aws_ami.example
 }
+
+data "aws_instance" "foo" {
+ instance_id = "i-07630329558f58805"
+ }
+output "instance" {
+ value = data.aws_instance.foo.public_ip
+}
