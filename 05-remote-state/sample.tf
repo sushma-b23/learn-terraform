f25be60e7fa5735b71184c0070fb825ae7e1f417
sushma-b23 <susmaboothu5@gmail.com>
2023-09-24 11:50:17 +0530

    jenkins

---
+++ 05-remote-state/sample.tf
@@ -0,0 +1,25 @@
+provider "aws" {
+    region  = "us-east-1"
+ }
+data "aws_ami" "centos8" {
+  most_recent = true
+  name_regex  = "Centos-8-DevOps-Practice"
+  owners      = ["973714476881"]
+}
+
+resource "aws_instance" "web" {
+   ami              = data.aws_ami.centos8.id
+   instance_type    = "t3.micro"
+
+   tags = {
+     Name = "test-centos8"
+  }
+}
+
+terraform {
+   backend "s3" {
+     bucket = "terraform-s70"
+     key    = "05-remote-state/terraform.tfstate"
+     region = "us-east-1"
+   }
+}
