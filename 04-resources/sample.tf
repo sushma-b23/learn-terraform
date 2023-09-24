ae6da3663346aaf0958ce443e6b05233119891a7
sushma-b23 <susmaboothu5@gmail.com>
2023-09-24 10:28:56 +0530

    jenkins

--- 04-resources/sample.tf
+++ 04-resources/sample.tf
@@ -1,7 +1,7 @@
 provider "aws" {
     region  = "us-east-1"
  }
-data "aws_ami" "example" {
+data "aws_ami" "centos8" {
   most_recent = true
   name_regex  = "Centos-8-DevOps-Practice"
   owners      = ["973714476881"]
