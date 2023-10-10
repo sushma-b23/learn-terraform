7ea9605452c8eb55bdada56c32d99e0aa3ae7447
sushma-b23 <susmaboothu5@gmail.com>
2023-10-10 15:19:23 +0530

    jenkins

---
+++ 09-conditions/main.tf
@@ -0,0 +1,17 @@
+resource "aws_instance" "web" {
+   ami     = data.aws_ami.centos8.id
+   instance_type  = var.type == "null" ? "t3.micro" : var.type
+
+   tags = {
+    Name = "demo"
+   }
+}
+
+data "aws_ami" "centos8" {
+    most_recent  = true
+    name_regex   = "centos-8-DevOps-Practice"
+    owners       = ["973714476881"]
+}
+variable "type" {
+    default  = "null"
+}
