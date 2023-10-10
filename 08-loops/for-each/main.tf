576ee371a980651100a0d826122daee3951a8512
sushma-b23 <susmaboothu5@gmail.com>
2023-10-10 13:17:04 +0530

    jenkins

--- 08-loops/for-each/main.tf
+++ 08-loops/for-each/main.tf
@@ -1,11 +1,11 @@
 resource "aws_instance" "web" {
-   count            = length(var.components)
+   for_each            = var.components
    ami              = data.aws_ami.centos8.id
    instance_type    = "t3.micro"
 
 
    tags = {
-     Name = "test"
+     Name = each.value["name"]
   }
 }
 
@@ -15,12 +15,12 @@ data "aws_ami" "centos8" {
   owners      = ["973714476881"]
 }
 
-output "publicip" {
-  #without count
-  #value = aws_instance.web.public_ip
-  #with count
-   value = aws_instance.web.*.public_ip
-}
+#output "publicip" {
+#  #without count
+#  #value = aws_instance.web.public_ip
+#  #with count
+#   value = aws_instance.web.*.public_ip
+#}
 
 variable "components" {
       default = {
