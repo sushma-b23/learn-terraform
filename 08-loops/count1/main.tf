6c29106f65c049968cbc437217d95efe3dea5ff7
sushma-b23 <susmaboothu5@gmail.com>
2023-10-10 12:42:00 +0530

    jenkins

--- 08-loops/count1/main.tf
+++ 08-loops/count1/main.tf
@@ -1,35 +1,36 @@
- resource "aws_instance" "web" {
-    count            = length(var.components)
-    ami              = data.aws_ami.centos8.id
-    instance_type    = "t3.micro"
+resource "aws_instance" "web" {
+   count            = length(var.components)
+   ami              = data.aws_ami.centos8.id
+   instance_type    = "t3.micro"
 
-    tags = {
-      Name = var.componennts[count.index]
-   }
- }
 
- data "aws_ami" "centos8" {
-   most_recent = true
-   name_regex  = "Centos-8-DevOps-Practice"
-   owners      = ["973714476881"]
- }
+   tags = {
+     Name = var.componennts[count.index]
+  }
+}
 
- output "publicip" {
-   #without count
-   #value = aws_instance.web.public_ip
-   #with count
-    value = aws_instance.web.*.public_ip
- }
+data "aws_ami" "centos8" {
+  most_recent = true
+  name_regex  = "Centos-8-DevOps-Practice"
+  owners      = ["973714476881"]
+}
 
- variable "components" {
-       default = {
-         cart  = {
-            name = "cart",
-            instance_type = "t3.small"
+output "publicip" {
+  #without count
+  #value = aws_instance.web.public_ip
+  #with count
+   value = aws_instance.web.*.public_ip
+}
+
+variable "components" {
+      default = {
+         cart = {
+           name = "cart",
+           instance_type = "t3.small"
          }
          catalogue = {
-            name = "catalogue",
-            instance_type = "t3.micro"
-         }
-       }
- }
+                    name = "catalogue",
+                    instance_type = "t3.micro"
+                  }
+      }
+}
