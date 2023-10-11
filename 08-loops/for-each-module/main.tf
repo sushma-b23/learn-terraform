ff8fe7686f09f3b6fa4631b532cd2908429fe04e
sushma-b23 <susmaboothu5@gmail.com>
2023-10-11 11:05:42 +0530

    update

--- 08-loops/for-each-module/main.tf
+++ 08-loops/for-each-module/main.tf
@@ -23,5 +23,7 @@ module "ec2" {
 ## Always map the data and use for_each loop
 
 output "publicip" {
-     value = module.ec2
+     value = {
+         for k, v in module.ec2 : k => v["ec2"].public_ip
+     }
 }
