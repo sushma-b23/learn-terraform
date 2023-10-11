c1e86731733857ad0bfb7070e6e6b980d3dd2e1e
sushma-b23 <susmaboothu5@gmail.com>
2023-10-11 23:01:25 +0530

    update

--- 11-provisioners/main.tf
+++ 11-provisioners/main.tf
@@ -11,7 +11,7 @@ data "aws_ami" "centos8" {
 resource "aws_instance" "web" {
    ami              = data.aws_ami.centos8.id
    instance_type    = "t3.micro"
-   vpc_security_group_ids = [aws_security_group.allow_tls.id]
+   vpc_security_group_ids = [aws_security_group.allow_tls1.id]
 
    tags = {
      Name = "test-centos8"
