76ab1df9784c37177f19eea4f55676b71e8445d1
sushma-b23 <susmaboothu5@gmail.com>
2023-10-11 22:55:59 +0530

    update

--- 11-provisioners/main.tf
+++ 11-provisioners/main.tf
@@ -34,25 +34,27 @@ resource "null_resource" "provision" {
 }
 
 resource "aws_security_group" "allow_tls" {
-   name   = "allow_tls"
-   description = "Allow TLS inbound traffic"
+       name  = "allow_tls"
+       description  = "Allow TLS inbound traffic"
 
-   ingress {
-      description = "TLS from VPC"
-      from_port = 22
-      to_port = 22
-      protocol = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
-   }
-   egress {
-     from_port = 0
-     to_port = 0
-     protocol = "-1"
-     cidr_blocks = ["0.0.0.0/0"]
-     ipv6_cidr_blocks = ["::/0"]
-   }
+       ingress  {
+         description  = "TLS from VPC"
+         from_port = 22
+         to_port = 22
+         protocol = "tcp"
+         cidr_blocks = ["0.0.0.0/0"]
+     }
+     egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+        ipv6_cidr_blocks = ["::/0"]
+     }
+     tags = {
+        Name = "allow_tls"
 
-   tags = {
-     Name = "allow_tls"
-   }
+     }
 }
+
+
