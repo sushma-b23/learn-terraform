d602a85d221bdacd2d7d7b2e060a73b9dac7418d
sushma-b23 <susmaboothu5@gmail.com>
2023-10-11 22:53:22 +0530

    update

--- 11-provisioners/main.tf
+++ 11-provisioners/main.tf
@@ -3,19 +3,21 @@ provider "aws" {
 }
 
 data "aws_ami" "centos8" {
-    most_recent = true
-    name_regex = "Centos8-DevOps-Practice"
-    owners     = ["973714476881"]
+  most_recent = true
+  name_regex  = "Centos-8-DevOps-Practice"
+  owners      = ["973714476881"]
 }
-resource "aws_instance" "web" {
-    ami        = data.aws_ami.centos8.id
-    instance_type   = "t3.micro"
-    vpc_security_group_ids = [aws_security_group.allow_tls.id]
 
-    tags = {
-       Name = "test-centos8"
+resource "aws_instance" "web" {
+   ami              = data.aws_ami.centos8.id
+   instance_type    = "t3.micro"
+   vpc_security_group_ids = [aws_security_group.allow_tls.id]
+
+   tags = {
+     Name = "test-centos8"
+  }
 }
-}
+
 
 resource "null_resource" "provision" {
    provisioner "remote-exec" {
