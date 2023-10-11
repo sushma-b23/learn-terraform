8555597959a8de5065fc2613e453c6a27339e3f6
sushma-b23 <susmaboothu5@gmail.com>
2023-10-11 22:19:11 +0530

    update

--- 11-provisioners/main.tf
+++ 11-provisioners/main.tf
@@ -36,7 +36,7 @@ resource "aws_security_group" "allow_tls" {
    description = "Allow TLS inbound traffic"
 
    ingress {
-      description = "TLS" from VPC"
+      description = "TLS from VPC"
       from_port = 22
       to_port = 22
       protocol = "tcp"
