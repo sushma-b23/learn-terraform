b5505a416d0b5acc1820c988b2b142cb962f4cbd
sushma-b23 <susmaboothu5@gmail.com>
2023-10-11 10:43:08 +0530

    update

---
+++ 08-loops/for-each-module/main.tf
@@ -0,0 +1,23 @@
+variable "components" {
+     default = {
+        cart = {
+          name = "cart",
+          instance_type "t3.small"
+        }
+
+        catalogue = {
+          name = "catalogue",
+          instance_type = "t3.micro"
+        }
+     }
+}
+
+module "ec2" {
+  source = "./module"
+  for_each = var.components
+  instance_type = each.value.instance_type
+  name = each.value.name
+}
+
+## Always iterate module, not resources
+## Always map the data and use for_each loop
---
+++ 08-loops/for-each-module/module/main.tf
@@ -0,0 +1,28 @@
+resource "aws_instance" "web" {
+
+   ami              = data.aws_ami.centos8.id
+   instance_type    = var.instance_type
+
+
+   tags = {
+     Name = var.name
+  }
+}
+
+data "aws_ami" "centos8" {
+  most_recent = true
+  name_regex  = "Centos-8-DevOps-Practice"
+  owners      = ["973714476881"]
+}
+
+//output "publicip" {
+//  #without count
+//  #value = aws_instance.web.public_ip
+//  #with count
+//  value = {
+//    for k, v in aws_instance.web : k => v.public_ip
+//  }
+//}
+
+variable "instance_type" {}
+variable "name" {}
